593c88db089b57ca8ec90708591b1b543bc584df
shirring92 <53874238+shirring92@users.noreply.github.com>
2020-02-20 14:40:44 -0600

    1st review
    https://www.lintcode.com/problem/partition-array/description

---
+++ 31. Partition Array.java
@@ -0,0 +1,100 @@
+31. Partition Array
+Given an array nums of integers and an int k, partition the array (i.e move the elements in "nums") such that:
+
+All elements < k are moved to the left
+All elements >= k are moved to the right
+Return the partitioning index, i.e the first index i nums[i] >= k.
+
+Example
+Example 1:
+
+Input:
+[],9
+Output:
+0
+
+Example 2:
+
+Input:
+[3,2,2,1],2
+Output:1
+Explanation:
+the real array is[1,2,2,3].So return 1
+Challenge
+Can you partition the array in-place and in O(n)?
+
+Notice
+You should do really partition in array nums instead of just counting the numbers of integers smaller than k.
+
+If all elements in nums are smaller than k, then return nums.length
+
+
+//two pointers
+public class Solution {
+    /**
+     * @param nums: The integer array you should partition
+     * @param k: An integer
+     * @return: The index after partition
+     */
+    public int partitionArray(int[] nums, int k) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int index = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] < k) {
+                swap(nums, i, index);
+                index++;
+            }
+        }
+        
+        return index;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+//quick sort
+public class Solution {
+    /**
+     * @param nums: The integer array you should partition
+     * @param k: An integer
+     * @return: The index after partition
+     */
+    public int partitionArray(int[] nums, int k) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int left = 0;
+        int right = nums.length-1;
+        while (left <= right) {
+            while (left <= right && nums[left] < k) {
+                left++;
+            }
+            while (left <= right && nums[right] >= k) {
+                right--;
+            }
+            if (left <= right) {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+        
+        return left;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
