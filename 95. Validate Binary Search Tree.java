02d9140f3de1a1bebe5d3e21b6be102f8ea51945
shirring92 <53874238+shirring92@users.noreply.github.com>
2020-03-08 21:55:15 -0500

    Update 95. Validate Binary Search Tree.java

--- 95. Validate Binary Search Tree.java
+++ 95. Validate Binary Search Tree.java
@@ -95,6 +95,49 @@ public class Solution {
     }
 }
 
+//recursion, divide & conquer, top-down
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    public boolean isValidBST(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return true;
+        }
+        
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean dfs(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        
+        boolean left = dfs(root.left, min, root.val);
+        boolean right = dfs(root.right, root.val, max);
+        
+        return left & right;
+    }
+}
+
 //iteration
 /**
  * Definition of TreeNode:
