7c9dfbbe70e3113e4a516e383f1b777d65a23141
shirring92 <53874238+shirring92@users.noreply.github.com>
2020-03-16 12:51:07 -0500

    1st review
    https://www.lintcode.com/problem/n-queens/description

--- 33. N-Queens.java
+++ 33. N-Queens.java
@@ -72,15 +72,14 @@ public class Solution {
     
     private boolean check(int col, int row, List<Integer> list) {
         for (int i=0; i<row; i++) {
-            if (list.get(i) == col) {
+            if (list.get(i) == col) {	//check column
                 return true;
             }
-        }
-        for (int i=0; i<row; i++) {
-            if (list.get(i) + i == col + row) {
+			
+            if (list.get(i) + i == col + row) {	//check diagonal line
                 return true;
             }
-            if (list.get(i) - i == col - row) {
+            if (list.get(i) - i == col - row) {	//check diagonal line
                 return true;
             }
         }
@@ -93,6 +92,81 @@ public class Solution {
             String str = "";
             for (int j=0; j<list.size(); j++) {
                 if (list.get(i) == j) {
+                    str += "Q";
+                }
+                else {
+                    str += ".";
+                }
+            }
+            ans.add(str);
+        }
+        
+        return ans;
+    }
+}
+
+//iteration. O(n^3). O(n^2)
+public class Solution {
+    /*
+     * @param n: The number of queens
+     * @return: All distinct solutions
+     */
+    public List<List<String>> solveNQueens(int n) {
+        // write your code here
+        List<List<String>> ans = new ArrayList<>();
+        if (n <= 0) {
+            return ans;
+        }
+        
+        Stack<List<Integer>> stack = new Stack<List<Integer>>();
+        for (int i=0; i<n; i++) {
+            List<Integer> list = new ArrayList<>();
+            list.add(i);
+            stack.push(list);
+        }
+        
+        while (!stack.isEmpty()) {
+            List<Integer> list = stack.pop();
+            if (list.size() == n) {
+                ans.add(build(list));
+                continue;
+            }
+            for (int i=0; i<n; i++) {
+                if (check(i, list)) {
+                    continue;
+                }
+                list.add(i);
+                stack.push(new ArrayList<>(list));
+                list.remove(list.size()-1);
+            }
+        }
+        
+        return ans;
+    }
+    
+    private boolean check(int col, List<Integer> list) {
+        for (int i=0; i<list.size(); i++) {
+            int precol = list.get(i);
+            if (col == precol) {
+                return true;
+            }
+            if (col + list.size() == precol + i) {
+                return true;
+            }
+            if (col - list.size() == precol - i) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private List<String> build(List<Integer> list) {
+        List<String> ans = new ArrayList<>();
+        for (int i=0; i<list.size(); i++) {
+            String str = "";
+            for (int j=0; j<list.size(); j++) {
+                if (j == list.get(i)) {
                     str += "Q";
                 }
                 else {
